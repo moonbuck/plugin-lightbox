0a23d4e22c1f0c9a767076fe7e746260128fd675
Jason Cardwell <moonbuck@users.noreply.github.com>
2021-10-25 14:41:13 -0700

    Syncing live changes, still playing around

--- static/assets/js/lightbox.js
+++ static/assets/js/lightbox.js
@@ -1,6 +1,32 @@
+const customLightboxHTML = `<div id="glightbox-body" class="glightbox-container">
+    <div class="gloader visible"></div>
+    <div class="goverlay"></div>
+    <div class="gcontainer">
+    <div id="glightbox-slider" class="gslider"></div>
+    <button class="gnext gbtn" tabindex="0" aria-label="Next" data-customattribute="example">{nextSVG}</button>
+    <button class="gprev gbtn" tabindex="1" aria-label="Previous">{prevSVG}</button>
+    <button class="gclose gbtn" tabindex="2" aria-label="Close">{closeSVG}</button>
+</div>
+</div>`;
+
+let customSlideHTML = `<div class="gslide">
+    <div class="gslide-inner-content">
+        <div class="ginner-container">
+            <div class="gslide-media">
+            </div>
+            <div class="gslide-description">
+                <div class="gdesc-inner">
+                    <h4 class="gslide-title"></h4>
+                    <div class="gslide-desc"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>`;
+    
 const lightbox = GLightbox({
-    touchNavigation: true,
-    touchFollowAxis: true,
-    keyboardNavigation: true,
-    closeButton: true
+  lightboxHTML: customLightboxHTML,
+  slideHTML: customSlideHTML,
+  width: '90%',
+  height: '90%'
 });

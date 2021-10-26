1005d1c1c6f5b6cf52a217d815eba707290d8cde
Jason Cardwell <moonbuck@users.noreply.github.com>
2021-10-26 07:23:01 -0700

    Renamed slide change event

--- static/assets/js/lightbox.js
+++ static/assets/js/lightbox.js
@@ -45,12 +45,12 @@ lightbox.on('slide_changed', ({ prev, current }) => {
   
   // Prev and current are objects that contain the following data
   // const { slideIndex, slideNode, slideConfig, player, trigger } = current;
-  let event = 'lightbox_click';
+  let event = 'lightbox_slide_changed';
   let title = current.slideConfig.title;
   let alt = current.slideConfig.alt;
   let description = current.slideConfig.description;
   let type = current.slideConfig.type;
   
-  dataLayer.push({'event': event,'title': title,'art_piece':title,'alt': alt,'description': description,'type': type});
+  dataLayer.push({'event': event,'slide_title': title,'art_piece':title,'slide_alt': alt,'slide_description': description,'slide_type': type});
   
 });
